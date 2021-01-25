e20711bef2c659d952e05086a9b666e9bf0cab3b
Francisco Diaz <fran.diaz.rio11@gmail.com>
2021-01-25 18:10:44 -0400

    changed proto definition and added node api functions

--- src/cmd/internal/node.go
+++ src/cmd/internal/node.go
@@ -12,76 +12,81 @@ import (
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
-
 type NodeAgentServer struct{}
 
 // Node type provides definition of a chord node
-type Node struct{
-	ID string
-	IpAddr string
-	nodeAgent NodeAgentServer
-	grpcServer *grpc.Server
-	listener *net.Listener
-	port int
-	virtualNode bool
-	connectionPool map[string]grpc.ClientConn	
-	
+type Node struct {
+	ID             string
+	IpAddr         string
+	nodeAgent      NodeAgentServer
+	grpcServer     *grpc.Server
+	listener       *net.Listener
+	port           int
+	connConfig     []grpc.DialOption
+	virtualNode    bool
+	connectionPool map[string]grpc.ClientConn
 }
 
-
-
-func (node *Node) IsVirtualNode()bool{
+func (node *Node) IsVirtualNode() bool {
 	return node.virtualNode
 }
 
-func NewNode(ID, IpAddr string, port int, virtualNode bool )*Node{
-	
-	if ipAddr:= net.ParseIP(IpAddr); ipAddr != nil{
+func NewNode(ID, IpAddr string, port int, virtualNode bool, configs ...grpc.DialOption) *Node {
+
+	if ipAddr := net.ParseIP(IpAddr); ipAddr != nil {
 		log.Fatalln("Invalid ip address")
 	}
 	
-
-
+	config := make([]grpc.DialOption, 2)
+	for _,cfg := range configs{
+		config = append(config, cfg)
+	}
 	return &Node{
-		ID: ID,
-		IpAddr: IpAddr,
-		grpcServer: grpc.NewServer(),
-		listener: nil,
-		port: port,
-		virtualNode: virtualNode,
+		ID:             ID,
+		IpAddr:         IpAddr,
+		grpcServer:     grpc.NewServer(),
+		listener:       nil,
+		port:           port,
+		connConfig:    	config, 
+		virtualNode:    virtualNode,
 		connectionPool: make(map[string]grpc.ClientConn),
 	}
 }
 
-func (node *Node) Start(){
+// Start node service by binding to the assigned address to the node
+func (node *Node) Start()*Node {
 	var err error
-	*node.listener,  err = net.Listen("tcp", node.IpAddr)
+	*node.listener, err = net.Listen("tcp", node.IpAddr)
 
-	if err != nil{
+	if err != nil {
 		// error creating listener on address
 		log.Fatal(err)
 	}
-	
-	// Using the grpc server in node 
+
+	// Using the grpc server in node
 	// Node implements the NodeAgentServer interface so we can use directly in here to start the service
 	pb.RegisterNodeAgentServer(node.grpcServer, &node.nodeAgent)
-	if err = node.grpcServer.Serve(*node.listener); err != nil{
+	if err = node.grpcServer.Serve(*node.listener); err != nil {
 		log.Fatal("failed to serve %v", err)
 	}
+
+	return node
 }
 
-func NewDefaultNode(ID string, port int)*Node{
-	return NewNode(ID, "localhost", port, false)	
+func (node *Node) Connect(targetNode *Node){
+	grpc.Dial(targetNode.IpAddr,node.connConfig...)
 }
 
 
-func (nodeAgent *NodeAgentServer) EchoReply(ctx context.Context, pingMsg *pb.PingMessage)(*pb.PingMessage, error){
+
+func NewDefaultNode(ID string, port int) *Node {
+	return NewNode(ID, "localhost", port, false)
+}
+
+func (nodeAgent *NodeAgentServer) EchoReply(ctx context.Context, pingMsg *pb.PingMessage) (*pb.PingMessage, error) {
 	log.Println("RCV - CONTENT: ", pingMsg.Info)
 	return &pb.PingMessage{
-		Info: "Echo Message",
-		Timestamp: timestamppb.Now(),			
+		Info:      "Echo Message",
+		Timestamp: timestamppb.Now(),
 	}, nil
-} 
-
-
-
+}
