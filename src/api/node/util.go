d28180c8722a0f1c2e52322dbd460b692763d780
Francisco Diaz <fran.diaz.rio11@gmail.com>
2021-01-31 14:05:10 -0400

    Added util test

--- src/api/node/util.go
+++ src/api/node/util.go
@@ -12,6 +12,19 @@ func isEqual(a, b []byte)bool{
 	return bytes.Compare(a, b) == 0
 }
 
+//Compares if key is between (a,b)
+func isBetween(key, a, b []byte)bool{
+	switch bytes.Compare(a, b){
+	case 0:  // a, b are Equal, key cannot be the same as a b by definition
+		return false 
+	case 1: // a > b, or b < a implies key > b and key < a  
+			return bytes.Compare(key, b)==1 && bytes.Compare(key, a)==-1
+	case -1: // a < b implies key < b and key > a
+			return bytes.Compare(key, b) == -1 && bytes.Compare(key, a)== 1
+	}
+
+	return false
+}
 func isPowerOfTwo(num int) bool{
 	return (num !=0 ) && ( (num & (num-1))==0)
 }
---
+++ src/api/node/util_test.go
@@ -0,0 +1,54 @@
+package node
+
+import (
+	"testing"
+
+)
+
+
+func TestHashEquals(t *testing.T){
+	a := "A"
+	b := "B"
+	eq := isEqual([]byte(a), []byte(b))
+	if eq{
+		t.Errorf("Method is incorrect: got A and B are %t when it is false", eq)
+	}
+}
+
+func TestInBetween(t *testing.T){
+	a := "a"
+	b := "b"
+	c := "c"
+
+
+	bet:=isBetween([]byte(b), []byte(a),[]byte(c))
+	
+	if !bet{
+		t.Error("hash of b should be between a and c ")
+	}
+
+
+	if isBetween([]byte(a), []byte(b), []byte(b)){
+		t.Errorf("hash a cannot be between two identical ends of a set") 
+	}
+
+	if !isBetween([]byte(b), []byte(c), []byte(a)){
+		t.Errorf("hash of b should be between c and a (reversed)")
+	}
+}
+
+
+
+func TestPowerOfTwo(t *testing.T){
+	num := 1
+	res := true
+	for num <= 1024{
+		res = res && isPowerOfTwo(num)
+		num *= 2
+	}
+
+	if !res{
+		t.Errorf("Error in power of two function")
+	}
+}
+
